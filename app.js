c00840c99ec8badbaf9fe8f696fa03869310d597
Adriano <adrianoo.rdg@gmail.com>
2022-11-24 14:56:47 -0300

    refactor and fix: apagando comentarios das rotas em ./app e arrumando comandos no package.json

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 require('express-async-errors');
 const express = require('express');
-// const productsController = require('./controllers/productsController');
-// const salesController = require('./controllers/salesController');
-// const validate = require('./middlewares/validations');
-// const search = require('./middlewares/searchProducts');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const mainRouter = require('./routes');
 
@@ -16,23 +12,6 @@ app.get('/', (_request, response) => {
 });
 
 app.use(mainRouter);
-// app.get('/products/search', productsController.getProductByQuery);
-// app.get('/products', productsController.getProducts);
-// app.post('/products', validate.nameValidate, productsController.addProduct);
-// app.get('/products/:id', productsController.getProductById);
-// app.put('/products/:id', validate.nameValidate, productsController.updateProduct);
-// app.delete('/products/:id', productsController.deleteProduct);
-
-// app.post('/sales', validate.productsValidate,
-//   search.verifyProductsExistence, salesController.addSales);
-
-// app.put('/sales/:id', validate.productsValidate,
-//   search.verifyProductsExistence, salesController.updateSales);
-
-// app.get('/sales', salesController.getAllSales);
-// app.get('/sales/:id', salesController.getSalesById);
-// app.delete('/sales/:id', salesController.deleteSale);
-
 app.use(errorMiddleware);
 
 module.exports = app;
